8b349499e5c1148345a299ea4335b4e7eb35da7a
Victor Torres <vpaivatorres@gmail.com>
2018-12-18 11:33:51 -0300

    make deploy script use production environment

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-jekyll build && rsync -avP _site/* victorjdh@victortorres.net.br:/var/www/victortorres.net.br/public_html/
+JEKYLL_ENV=production jekyll build && rsync -avP _site/* victorjdh@victortorres.net.br:/var/www/victortorres.net.br/public_html/
